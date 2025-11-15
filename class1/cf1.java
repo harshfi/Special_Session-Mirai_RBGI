5a15ae129e11b33605c2795c09a286f3b655ffb7
harshfi <harshcooltripathi1@gmail.com>
2025-11-15 17:12:32 +0530

    codeforces and kadanes Algo

--- class1/cf1.java
+++ class1/cf1.java
@@ -1,5 +1,3 @@
-package lec1;
-
 import java.util.Scanner;
 
 public class cf1 {
@@ -11,54 +9,28 @@ public class cf1 {
 		int t=sc.nextInt();
 		
 		while(t-->0) {
-			int n=sc.nextInt();
+			int n=sc.nextInt();//cin>>n;
 			
-			int min=Integer.MAX_VALUE;
+			int k=sc.nextInt();//cin>>k;
 			
-			int max=Integer.MIN_VALUE;
+			int prev=Integer.MIN_VALUE;  //Int_min
 			
-			int arr[]= new int[n];
-			
-			for(int i=0;i<n;i++) {
+			boolean sort=true;
+			for (int i = 0; i <n; i++) {
+				int a=sc.nextInt(); //cin>>a;
 				
-				int a=sc.nextInt();
-				arr[i]=a;
-				if(i==0) {
-					max=0;
-					min=0;
+				if(prev>a) {
+					sort=false;
 				}
-				
-				if(arr[max]<arr[i]) {
-					max=i;
-				}
-				
-				if(arr[min]>arr[i]) {
-					min=i;
-				}
+				prev=a;
 			}
 			
-			if((arr[max]-arr[min])==0)System.out.println("No");
-			
+			if(k>=2 ||sort==true) {
+				System.out.println("Yes");// cout<<"Yes";
+			}
 			else {
-				
-				System.out.println("Yes");
-				System.out.print(arr[max]+" "+arr[min]+" ");
-				for(int i=0;i<arr.length;i++) {
-					
-					if(i==max || i==min)continue;
-					
-					System.out.print(arr[i]+" ");
-				}
-				
-				System.out.println();
-				
+				System.out.println("No");//cout<<"No";
 			}
-			
-			
-			
-			
-			
-			
 			
 		}
 		
